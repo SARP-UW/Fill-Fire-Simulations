e51b4619e3b89aac68366da17129eb17e370096f
gbh2022UW <gbh2022@uw.edu>
2025-10-06 19:38:30 -0700

    Starting mdot tank orifice

---
+++ functions/mdot_tank_orifice.m
@@ -0,0 +1,7 @@
+pressure_atm = 14.7; %PSI
+
+function output = get_mdot_tank_orifice(pressure_tank, orifice_diameter)
+    pressure_ratio = pressure_tank / pressure_atm;
+    
+    output = 69;
+end
