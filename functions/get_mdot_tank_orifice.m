b66cf19fcbf4514206b2b819ad82ae6cc1433b56
gbh2022UW <gbh2022@uw.edu>
2025-10-14 19:17:58 -0700

    Zach added comments

--- functions/get_mdot_tank_orifice.m
+++ functions/get_mdot_tank_orifice.m
@@ -7,6 +7,9 @@ function [m_dot, Fill_Time] = get_mdot_tank_orifice(pressure_tank, orifice_diame
 % orfice_diameter: Size of tubing that will connect to the nitrous tank
 % Cylinder Diameter: Size of cylinder output
 
+% Outpus
+% m_dot: mass flow rate
+% Fill time: time to fill tank
 arguments (Input)
     pressure_tank (1,1) double{mustBeReal, mustBeFinite, mustBePositive} %psi
     orifice_diameter (1,1) double{mustBeReal, mustBeFinite, mustBePositive} % in
@@ -18,8 +21,7 @@ arguments (Output)
     Fill_Time (1,1) double{mustBeReal, mustBeFinite, mustBePositive} % s
 end 
 
-% Constants / Future Inputs??
-
+% Constants
 P_Initial = 14.7; %psi
 Molar_Density_Initial = 27.958; % mol/l^3
 k = 1.27; % Specific heat ratio
@@ -29,13 +31,21 @@ Vol_Tank = 0.3266606672; %from thrust simulation
 
 % Calculations
 density_initial = Molar_Density_Initial * (44.013)/(453.592*0.035315);
+% density (lb/ft^3) = Molar_Desnity (mol/L) * (453.592 g / mol) * (1 lb / 453.592 g) * (1 L / 0.035315 ft^3) 
 Beta = orifice_diameter/cylinder_diam;
+% diameter ratios
 P_ratio = pressure_tank/P_Initial;
+% Pressure Ratios
 Y = (((k * (P_ratio)^(2/k))/(k-1)) * ((1-Beta^4)/((1-Beta^4)*(P_ratio)^(2/k)))...
     *((1-(P_ratio)^((k-1)/(k))) / (1 - P_ratio)))^(0.5);
+% net expansion factor for compressible flow through orifices, nozzles, venturi, control valves or pipe (unitless)
 C = Cd/sqrt(1-Beta^4);
+% flow coefficient for orifices and nozzles
 A = pi * (orifice_diameter/2)^2;
+% Area
 q = Y*C*A * sqrt((2*g*144*pressure_tank - P_Initial)/density_initial);%ft^3/s
+% rate of flow at flowing conditions
+
 m_dot = q*density_initial;
 
 Fill_Time = Vol_Tank / q;
