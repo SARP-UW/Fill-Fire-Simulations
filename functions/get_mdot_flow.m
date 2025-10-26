5d3554f82b9e00723b74040476ca96af9b127a11
emersonbeehler <emersonbeehler@gmail.com>
2025-10-26 10:24:52 -0700

    Abstracted K_total into separate function

--- functions/get_mdot_flow.m
+++ functions/get_mdot_flow.m
@@ -1,15 +1,17 @@
 %% Calculates mdot from pressure difference across fill 
 % View full documentation here: https://www.overleaf.com/read/yzpnyzrksypj#0a7d39
 
-function mdot_flow = get_mdot_flow(P1, P2, f, K_total, L, D, rho)
+function mdot_flow = get_mdot_flow(P1, P2, f, K_total, rho)
     % P1 = pressure in K bottle (Pa)
     % P2 = pressure in run tank (Pa)
     % f = friction factor (dimensionless)
     % K_total = total K-loss coefficients (dimensionless)
-    % L = length of piping (m)
-    % D = internal diameter of piping (m)
     % rho = density of nitrous (kg / m^3)
     % mdot_flow = mass flow of nitrous (kg / s)
+
+    % Define Constants
+    D = 10.2108 / 1000; % inner diameter of pipe (m)
+    L = ; % length of piping (m)
 
 
     % Calculate area (m^2):
